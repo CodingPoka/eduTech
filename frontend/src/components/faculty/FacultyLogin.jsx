86bb73b844603a98435a25ce8790dd502552e877
CodingPoka <kingshukbd.01@gmail.com>
2026-01-06 01:46:18 +0600

    just creating admin, faculty and student folder and it's necessary file

---
+++ frontend/src/components/faculty/FacultyLogin.jsx
@@ -0,0 +1,11 @@
+
+
+import React from 'react'
+
+const FacultyLogin = () => {
+  return (
+    <div>FacultyLogin</div>
+  )
+}
+
+export default FacultyLogin
---
+++ frontend/src/components/faculty/FacutlyProfile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FacutlyProfile = () => {
+  return (
+    <div>FacutlyProfile</div>
+  )
+}
+
+export default FacutlyProfile
